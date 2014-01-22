be888545706cbc665b4ab0f1033a7558164998cb
George Boumis <george.boumis@gmail.com>
2014-01-22 13:30:24 +0100

    Initial Commit

---
+++ pluginmanager/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pluginmanager
+//
+//  Created by George Boumis on 22/1/14.
+//  Copyright (c) 2014 George Boumis <developer.george.boumis@gmail.com>. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
